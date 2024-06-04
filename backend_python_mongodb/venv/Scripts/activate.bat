a27b6777119d317401b6c5ed1c090ca0b0924ff9
shivam sahu <41139996+shiv112@users.noreply.github.com>
2024-06-04 08:32:16 +0530

    shivam ui and bknd changes new

--- backend_python_mongodb/venv/Scripts/activate.bat
+++ backend_python_mongodb/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\FlaskProjectBE\venv
+set VIRTUAL_ENV=D:\Python\rent_your_property_2\backend_python_mongodb\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
